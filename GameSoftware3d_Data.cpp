2ce7fb8c92f60b6a40ba9b6d54f4f3d39f0b7666
ImGelling <47453119+imgelling@users.noreply.github.com>
2022-07-31 20:25:16 -0400

    Update GameSoftware3d_Data.cpp
    * changed name of variable, stm to stream

--- GameSoftware3d_Data.cpp
+++ GameSoftware3d_Data.cpp
@@ -7,18 +7,18 @@ RasterMode& operator++ (RasterMode& rmode, int)
 	if (rmode == RasterMode::None) rmode = static_cast<RasterMode>(0);
 	return rmode;
 }
-std::ostream& operator<< (std::ostream& stm, RasterMode rmode)
+std::ostream& operator<< (std::ostream& stream, RasterMode rmode)
 {
 	switch (rmode)
 	{
-	case RasterMode::WireFrame: return stm << "WireFrame";
-	case RasterMode::Filled: return stm << "Filled";
-	case RasterMode::WireFrameFilled: return stm << "WireFrame Filled";
-	case RasterMode::AffineTextureMapped: return stm << "Affine Texture Mapped";
-	case RasterMode::WireFrameAffTexture: return stm << "WireFrame Affine Texture Mapped";
-	case RasterMode::ProjectionTextureMapped: return stm << "Projection Correct Texture Mapped";
-	case RasterMode::WireFrameProjTexture: return stm << "WireFrame Projection Correct Texture Mapped";
-	default: return stm << "Unknown Enumerator";
+	case RasterMode::WireFrame: return stream << "WireFrame";
+	case RasterMode::Filled: return stream << "Filled";
+	case RasterMode::WireFrameFilled: return stream << "WireFrame Filled";
+	case RasterMode::AffineTextureMapped: return stream << "Affine Texture Mapped";
+	case RasterMode::WireFrameAffTexture: return stream << "WireFrame Affine Texture Mapped";
+	case RasterMode::ProjectionTextureMapped: return stream << "Projection Correct Texture Mapped";
+	case RasterMode::WireFrameProjTexture: return stream << "WireFrame Projection Correct Texture Mapped";
+	default: return stream << "Unknown Enumerator";
 	}
 }
 
@@ -28,12 +28,12 @@ RasterType& operator++ (RasterType& rType, int)
 	if (rType == RasterType::None) rType = static_cast<RasterType>(0);
 	return rType;
 }
-std::ostream& operator<< (std::ostream& stm, RasterType rmode)
+std::ostream& operator<< (std::ostream& stream, RasterType rmode)
 {
 	switch (rmode)
 	{
-	case RasterType::Barycentric: return stm << "Barycentric (Slower but smoother)"; 
-	case RasterType::ScanLine: return stm << "Scan Line (Faster but noisey)";
-	default: return stm << "Unknown Enumerator";
+	case RasterType::Barycentric: return stream << "Barycentric (Slower but smoother)"; 
+	case RasterType::ScanLine: return stream << "Scan Line (Faster but noisey)";
+	default: return stream << "Unknown Enumerator";
 	}
 }
